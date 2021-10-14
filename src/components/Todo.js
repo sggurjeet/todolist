14dde8206a2c187ae3a96949df41bd7f68f2bd17
sggurjeet <91647363+sggurjeet@users.noreply.github.com>
2021-10-14 15:56:44 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
-// import { RiCloseCircleLine } from "react-icons/ri";
-// import { TiEdit } from "react-icons/ti";
 
 const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
   const [edit, setEdit] = useState({
