e3f8c133a074889e40f6b771085a3b26258dba7b
sggurjeet <gurjeet@studiographene.com>
2021-10-14 15:54:41 +0530

    edit

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,13 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 
 function TodoForm(props) {
   const [input, setInput] = useState(props.edit ? props.edit.value : "");
-
-  const inputRef = useRef(null);
-
-  useEffect(() => {
-    inputRef.current.focus();
-  });
 
   const handleChange = (e) => {
     setInput(e.target.value);
@@ -15,9 +9,9 @@ function TodoForm(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    const count = 0;
     props.onSubmit({
-      id: Math.floor(Math.random() * 10000),
+      id: count + 1,
       text: input,
     });
     setInput("");
@@ -32,7 +26,6 @@ function TodoForm(props) {
             value={input}
             onChange={handleChange}
             name="text"
-            ref={inputRef}
             className="todo-input edit"
           />
           <button onClick={handleSubmit} className="todo-button edit">
@@ -47,7 +40,6 @@ function TodoForm(props) {
             onChange={handleChange}
             name="text"
             className="todo-input"
-            ref={inputRef}
           />
           <button onClick={handleSubmit} className="todo-button">
             Add todo
